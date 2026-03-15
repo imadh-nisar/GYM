0d50b2fe4c1c27a03bbd9646567109ba8b55c91d
imadh-nisar <pcimadh@gmail.com>
2026-03-15 23:10:51 +0530

    Delete Fitness guide directory

--- Fitness guide/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Handle login/register messages
-document.addEventListener("DOMContentLoaded", () => {
-  const params = new URLSearchParams(window.location.search);
-  if (params.has("msg")) {
-    alert(params.get("msg"));
-  }
-});
